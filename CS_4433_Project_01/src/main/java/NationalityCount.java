7241bedd5729e9a1f821ec3471a4fa6fa88eec46
Ronak Wani <117783872+ronak-wani@users.noreply.github.com>
2025-01-31 15:11:17 -0500

    Mapper Done

--- CS_4433_Project_01/src/main/java/NationalityCount.java
+++ CS_4433_Project_01/src/main/java/NationalityCount.java
@@ -11,7 +11,22 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
+import java.util.*;
+
 public class NationalityCount {
+
+    public static class CountryMapper extends Mapper<Object, Text, Text, Text> {
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            String line = value.toString();
+            String[] fields = line.split(",");
+            String name = fields[1].trim();
+            String nationality = fields[2].trim();
+            String hobby = fields[4].trim();
+            if (nationality.equalsIgnoreCase("Netherlands")) {
+                context.write(new Text(name), new Text(hobby));
+            }
+        }
+    }
     public void debug(String[] args) throws Exception {}
 
     public static void main(String[] args) throws Exception {}
