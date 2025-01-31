875d5a4110eec57d5687fef4e38b29b78f5a8da7
Ronak Wani <117783872+ronak-wani@users.noreply.github.com>
2025-01-31 15:38:15 -0500

    Ronak branch (#1)
    * Update .gitignore
    
    * Mapper Done
    
    * main and debug methods
    
    * gitignore updated
    
    * Update .gitignore

--- CS_4433_Project_01/src/main/java/NationalityCount.java
+++ CS_4433_Project_01/src/main/java/NationalityCount.java
@@ -11,10 +11,45 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public class NationalityCount {
-    public void debug(String[] args) throws Exception {}
+import java.util.*;
 
-    public static void main(String[] args) throws Exception {}
+public class NationalityCount {
+
+    public static class CountryMapper extends Mapper<Object, Text, Text, Text> {
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            String line = value.toString();
+            String[] fields = line.split(",");
+            String name = fields[1].trim();
+            String nationality = fields[2].trim();
+            String hobby = fields[4].trim();
+            if (nationality.equalsIgnoreCase("Netherlands")) {
+                context.write(new Text(name), new Text(hobby));
+            }
+        }
+    }
+    public void debug(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "Facebook Dataset");
+        job.setJarByClass(NationalityCount.class);
+        job.setMapperClass(CountryMapper.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "Facebook Dataset");
+        job.setJarByClass(NationalityCount.class);
+        job.setMapperClass(CountryMapper.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
 }
 
 
